5f6ea03991b2c59da75f97e406fbc2b59f7f4f47
hongyusu <hongyu.su.uh@gmail.com>
2013-02-28 12:20:13 +0200

    2802

--- autorun_mmpartial.sh
+++ autorun_mmpartial.sh
@@ -5,6 +5,7 @@ do
     for missingper = '0' '0.05' '0.15' '0.25' '0.5'
     do
         nohup matlab -nodesktop -nosplash -r "run_MMCRFpartial_exp1 '$inname' '$missingper'" &
+        sleep 0.5
     done
 done
 wait
